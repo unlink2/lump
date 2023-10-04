683d0cad51c325c9702b1a68b56195002b46883d
Lukas Krickl <lukas@krickl.dev>
2023-10-04 20:48:37 +0200

    Added md_ext ifdef

--- include/config.h
+++ include/config.h
@@ -1,10 +1,20 @@
 #ifndef CONFIG_H_
-#define CONFIG_H_ 
+#define CONFIG_H_
 
 /**
- * Commend this out to disable 
+ * Commend this out to remove all 
+ * markdown extensions
+ */
+#define LUMP_MD_EXT 1
+
+#ifdef LUMP_MD_EXT
+
+/**
+ * Commend this out to disable
  * shell execution in tempaltes
  */
 #define LUMP_ALLOW_EXEC 1
 
-#endif 
+#endif
+
+#endif
